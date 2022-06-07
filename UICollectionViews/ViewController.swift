240a00381aa9bd12f056cf2833ae825865fbde32
ricklarios <81334604+ricklarios@users.noreply.github.com>
2022-06-08 00:28:03 +0200

    Initial Commit

---
+++ UICollectionViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UICollectionViews
+//
+//  Created by Cuenta Personal on 8/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
