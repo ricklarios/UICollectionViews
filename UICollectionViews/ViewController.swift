8be2db18d7d0bb59547a4bf0c91baebae76c699a
ricklarios <81334604+ricklarios@users.noreply.github.com>
2022-06-08 00:32:58 +0200

    Add collection view

--- UICollectionViews/ViewController.swift
+++ UICollectionViews/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var myCollectionView: UICollectionView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
